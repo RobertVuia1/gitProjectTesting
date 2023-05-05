a616dd2c21513d8ba904c1652a252d8ba46e2440
Robert <robert.vuia03@e-uvt.ro>
2023-05-05 19:15:10 +0300

    TestRobert

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,9 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-        Testarefisier1 t1 = new Testarefisier1("testarea reusita");
-
-        System.out.println(t1.toString());
         
         //test
     }
